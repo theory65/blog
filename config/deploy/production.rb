7d0f9c73f4457fe42e1cad4ee8e7f3d40acd8aca
M <theory65@hotmail.com>
2015-08-16 04:05:39 -0400

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,8 @@
 # You can define all roles on a single server, or split them:
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-server '192.168.56.104', user: 'm', roles: %w{app web}
-server '192.168.56.103', user: 'm', roles: %w{db}
+# server '192.168.56.104', user: 'm', roles: %w{app web}
+# server '192.168.56.103', user: 'm', roles: %w{db}
 
 
 
@@ -17,9 +17,9 @@ server '192.168.56.103', user: 'm', roles: %w{db}
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{m@192.168.56.104}
+role :web, %w{m@192.168.56.104}
+role :db,  %w{m@192.168.56.104}
 
 
 
